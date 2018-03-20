02867ca418b59654af74e2bcdb24486aaa60c646
Md Al Amin <alamin.hawalder69@gmail.com>
2018-03-20 02:43:00 -0400

    method no:12

--- src/day/one/MethodPractice.java
+++ src/day/one/MethodPractice.java
@@ -8,8 +8,27 @@ public class MethodPractice {
 		
 		System.out.println("Q no-11"+ Arrays.toString(getNumber(20)));
 		
+		System.out.println("Q no-12"+ Arrays.toString(getNum(20)));
 		
 		
+		
+	}
+	
+	/**
+	 * Q no:12
+	 * write a method that take one number Array input and return an Array that contains 1 to given number
+	 * parameter:int number
+	 * returnType:int[]
+	 * Servingbucket:variable
+	 */
+	public static int[] getNum(int number){
+		int[] numArray = new int[number];
+		
+		for(int i = 1; i <= number; i++){
+			numArray[i-1] = i;
+		}
+		
+		return numArray;
 	}
 	
 	/**
@@ -26,8 +45,6 @@ public class MethodPractice {
 			
 			numberArray[i] = i;
 		}
-		
-		
 		return numberArray;
 	}
 
